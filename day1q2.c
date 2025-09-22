10f5404e4bacf3696e0575dc94cc49a0671ecf29
nikitaarora18 <nikitaaror6918@gmail.com>
2025-09-23 02:58:45 +0530

    Add files via upload

---
+++ day1q2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int a=10;
+    int b=2;
+    int c=7;int d=3;
+    int sum=a+b;
+    int sum1=c+d;
+    int diff=a-b;
+    int diff1=c-d;
+    int product=a*b;
+    int product1=c*d;
+    float quotient=a/b;
+    float quotient1=c/d;
+    printf("the sum of a and b is %d \n",sum);
+     printf("the sum of c and d is %d \n",sum1);
+    printf("the difference of a and b is %d \n",diff);
+    printf("the difference of c and d is %d \n",diff1);
+    printf("the product of a and b is %d \n",product);
+      printf("the product of c and d is %d \n",product1);
+    printf("the quotient of and b is %f \n",quotient);
+ printf("the quotient of c and d is %f \n",quotient1);
+    return 0;
+}
